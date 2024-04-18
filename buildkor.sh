c1e0a2b94fe19d704f1e80ae1d6e73243aabc2b2
Adonte Harrell <producedbyeskay@gmail.com>
2024-04-18 16:07:30 -0400

    Commit

---
+++ buildkor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Set enviroment variables
+VID_DIR="/videos"
+WIN_USER="Kizle"
+WIN_PW="james460"
+WIN_DIR=$(\\10.0.0.236\Videos2)
+
+#Install podman and enable service.
+yum install podman -y
+systemctl enable --now podman
+
+#Install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod 755 /usr/local/bin/docker-compose
+
+#Mount your Windows drive to Linux
+sudo mount -t cifs $WIN_DIR $VID_DIR -o username=$WIN_USER,password=$WIN_PW
+
+#Build korcese environment
+docker-compose up -d 
+#Start container and move movies
+podman exec -it korcese sh convert.sh 
